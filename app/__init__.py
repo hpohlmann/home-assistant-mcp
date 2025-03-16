9a8369d7604c3640c2c7f73e9b7a53c211c14dc6
tedostrem <ted.ostrem@gmail.com>
2025-03-16 13:10:22 +0000

    feat: expose color control function

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from .config import validate_config
 from .api import make_ha_request, get_all_entities
-from .tools import init_tools, control_device, search_entities
+from .tools import init_tools, control_device, search_entities, set_device_color
 from .utils import search_entities_by_keywords, format_entity_results
 
 __all__ = [
@@ -10,6 +10,7 @@ __all__ = [
     'init_tools',
     'control_device',
     'search_entities',
+    'set_device_color',
     'search_entities_by_keywords',
     'format_entity_results'
 ]
